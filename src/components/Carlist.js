b502f10a6dd2cd5b16736a1bc956f0382c0b5cc2
Pertzamus <korhonen.pertti@hotmail.com>
2022-04-19 19:57:32 +0300

    tehtävän alku

---
+++ src/components/Carlist.js
@@ -0,0 +1,19 @@
+import React, { useState, useEffect }  from "react";
+
+export default function Carlist() {
+    const [cars, setCars] = useState([]);
+
+    useEffect(() => fetchData(), []);
+
+    const fetchData = () => {
+        fetch('https://carstockrest.herokuapp.com/cars')
+        .then(response => response.json())
+        .then(data => setCars(data._embedded.cars))
+    }
+
+    return (
+        <div>
+
+        </div>
+    )
+}
